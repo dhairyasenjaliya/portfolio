addc42783cc0e311650b295f234e725be12610f0
dhairya <dhairyasenjaliya@gmail.com>
2019-04-23 18:26:12 +0530

    Fix

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://dhairyasenjaliya.github.io/portfolio/',
+    assetsPublicPath: 'https://dhairyasenjaliya.github.io/portfolio/dist/',
 
     /**
      * Source Maps
