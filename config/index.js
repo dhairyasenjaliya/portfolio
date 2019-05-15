b11e7a172ab47b847837dffbce4dcc3287e896a6
dhairya <dhairyasenjaliya@gmail.com>
2019-05-15 12:53:05 +0530

    Gh pages update

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://dhairyasenjaliya.github.io/portfolio/dist/',
+    assetsPublicPath: 'https://dhairyasenjaliya.github.io/portfolio/docs/',
 
     /**
      * Source Maps
